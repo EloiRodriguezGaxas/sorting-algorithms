4007e77b05b2154868ebb9861b30c5262e755029
Eloi Rodríguez <erodriguezg@edu.tecnocampus.cat>
2020-04-25 17:40:48 +0200

    quicksort start

---
+++ js/quicksort.js
@@ -0,0 +1,18 @@
+class Quicksort {
+    constructor(unsorted) {
+        this.unsorted = unsorted;
+    }
+
+    quicksort(low, high) {
+        if(low<high) {
+            this.partition((partitionIdx) => {
+
+            })
+        }
+    }
+
+    partition(low, high, callback) {
+        
+    }
+
+}
