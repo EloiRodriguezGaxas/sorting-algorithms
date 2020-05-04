a0c2ed91e460e41d8a7548e21be13773e026d097
Eloi Rodríguez <erodriguezg@edu.tecnocampus.cat>
2020-05-04 20:01:42 +0200

    small change

--- js/quicksort.js
+++ js/quicksort.js
@@ -1,43 +1,30 @@
-class Quicksort {
-    constructor(unsorted) {
-        console.log(unsorted);
-        this.sort(unsorted);
-    }
+[2,5,1,0]
+function quickSort(unsorted) {
+    sortedArray = _quicksort([...unsorted], 0, unsorted.length - 1);
+    console.log(sortedArray);
+}
 
-    async sort(unsorted) {
-        this.sortedArray = await this.quicksort([...unsorted], 0, unsorted.length - 1);
-        console.log(this.sortedArray);
+function _quicksort(unsorted, low, high) {
+    if (low < high) {
+        const partitionIdx = partition(unsorted, low, high);
+        _quicksort(unsorted, low, partitionIdx - 1);
+        _quicksort(unsorted, partitionIdx, high);
     }
+    return unsorted;
+}
 
-    async quicksort(unsorted, low, high) {
-        if (low < high) {
-            const partitionIdx = await this.partition(unsorted, low, high);
-            await this.quicksort(unsorted, low, partitionIdx - 1);
-            await this.quicksort(unsorted, partitionIdx, high);
+function partition(unsorted, low, high) {
+    var pivot = unsorted[high];
+    var i = low;
+    for (let j = low; j < high - 1; j++) {
+        if (unsorted[j] >= pivot) {
+            swap(unsorted, i, j);
+            i++;
         }
-        return unsorted;
     }
 
-    async partition(unsorted, low, high) {
-        var pivot = unsorted[high];
-        var i = low;
-        for (let j = low; j < high - 1; j++) {
-            if (unsorted[j] >= pivot) {
-                this.swap(unsorted, i, j);
-                i++;
-            }
-        }
+    swap(unsorted, i + 1, high);
 
-        this.swap(unsorted, i + 1, high);
-
-        return i;
-
-    }
-
-    swap(unsorted, i, j) {
-        var tmp = unsorted[i];
-        unsorted[i] = unsorted[j];
-        unsorted[j] = tmp;
-    }
+    return i;
 
 }
