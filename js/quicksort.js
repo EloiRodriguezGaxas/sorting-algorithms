7a0e8457da88a70ed82a71823f9ed3bd291e4ac5
Eloi Rodríguez <erodriguezg@edu.tecnocampus.cat>
2020-05-04 20:01:24 +0200

    quicksort

--- js/quicksort.js
+++ js/quicksort.js
@@ -1,18 +1,43 @@
 class Quicksort {
     constructor(unsorted) {
-        this.unsorted = unsorted;
+        console.log(unsorted);
+        this.sort(unsorted);
     }
 
-    quicksort(low, high) {
-        if(low<high) {
-            this.partition((partitionIdx) => {
+    async sort(unsorted) {
+        this.sortedArray = await this.quicksort([...unsorted], 0, unsorted.length - 1);
+        console.log(this.sortedArray);
+    }
 
-            })
+    async quicksort(unsorted, low, high) {
+        if (low < high) {
+            const partitionIdx = await this.partition(unsorted, low, high);
+            await this.quicksort(unsorted, low, partitionIdx - 1);
+            await this.quicksort(unsorted, partitionIdx, high);
         }
+        return unsorted;
     }
 
-    partition(low, high, callback) {
-        
+    async partition(unsorted, low, high) {
+        var pivot = unsorted[high];
+        var i = low;
+        for (let j = low; j < high - 1; j++) {
+            if (unsorted[j] >= pivot) {
+                this.swap(unsorted, i, j);
+                i++;
+            }
+        }
+
+        this.swap(unsorted, i + 1, high);
+
+        return i;
+
+    }
+
+    swap(unsorted, i, j) {
+        var tmp = unsorted[i];
+        unsorted[i] = unsorted[j];
+        unsorted[j] = tmp;
     }
 
 }
