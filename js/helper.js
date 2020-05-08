8ec9f54d9aee85a1e8eda19b6ea74a65166e89d6
Eloi Rodríguez <erodriguezg@edu.tecnocampus.cat>
2020-05-08 09:25:09 +0200

    improved move function

--- js/helper.js
+++ js/helper.js
@@ -1,22 +1,3 @@
-function swap(i, j, _arr) {
-    let tmp = _arr[i];
-    _arr[i] = _arr[j];
-    _arr[j] = tmp;
-}
-
-function move(i, j, _arr) {
-
-    if (i < 0 || i > _arr.length || j < 0 || j > _arr.length) {
-        console.log("Indices i (", i, ") and j (", j, ") must be within the limits of the array length (0, ", _arr.length, ")");
-        return _arr;
-    }
-
-    _arr.splice(j, 0, _arr.splice(i, 1)[0]);
-
-    return _arr;
-
-}
-
 Array.prototype.swap = function (i, j) {
     let tmp = this[i];
     this[i] = this[j];
@@ -24,5 +5,19 @@ Array.prototype.swap = function (i, j) {
 }
 
 Array.prototype.move = function (from, to) {
-    this.splice(to, 0, this.splice(from, 1)[0]);
+
+    let tmp = this[from];
+
+    if (from < to) {
+        for (let i = from; i < to; i++) {
+            this[i] = this[i + 1];
+        }
+        this[to] = tmp;
+    } else {
+        for (let i = from; i > to; i--) {
+            this[i] = this[i - 1];
+        }
+        this[to] = tmp;
+    }
+
 }
